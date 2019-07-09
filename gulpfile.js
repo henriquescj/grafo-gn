080d6aca4e8450972901b0ed5977781001facb0c
Marianna <marianna@rckt.com.br>
2019-07-08 22:42:42 -0300

    adicionando o html

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,12 @@ var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
 
 // regra para o sass virar css
-// gulp.task('css', function(){
-//   return gulp.src('scss/style.scss')
-//     .pipe(sass())
-//     .pipe(minifyCSS())
-//     .pipe(gulp.dest('css'))
-// });
+gulp.task('css', function(){
+  return gulp.src('scss/style.scss')
+    .pipe(sass())
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('css'))
+});
 
 // adicionar bibliotecas, lembrando que a ordem é importante, bootstrap precisa do jquery, então jquery tem que vir antes
 gulp.task('js', function(){
@@ -20,6 +20,7 @@ gulp.task('js', function(){
       'node_modules/sigma/build/plugins/sigma.plugins.animate.min.js',
       'node_modules/sigma/build/plugins/sigma.layout.noverlap.min.js',
       'node_modules/sigma/build/plugins/sigma.renderers.customEdgeShapes.min.js',
+      // 'node_modules/bootstrap/dist/js/bootstrap.min.js',
     ])
     .pipe(sourcemaps.init())
     .pipe(concat('script.js'))
@@ -36,17 +37,17 @@ gulp.task('js', function(){
 
 // regra geral que vale ao digitar apenas gulp
 // gulp.task('default', [ 'css', 'js', 'pages' ]);
-gulp.task('default', [ 'js' ]);
+gulp.task('default', [ 'css','js' ]);
 
 // regra para o gulp watch
 gulp.task('watch', function() {
 
   gulp.run('default');
 
-  // gulp.watch('scss/*.scss', function(event) {
-  //     console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-  //     gulp.run('css');
-  // });
+  gulp.watch('scss/*.scss', function(event) {
+      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+      gulp.run('css');
+  });
 
   gulp.watch('src/**/*.js', function(event) {
       console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
