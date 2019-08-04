6f9d6ffa663918a214bf459de45c7621f38a8aec
Henrique Junges <henrique@rckt.com.br>
2019-08-04 18:18:53 -0300

    filtro de palavras e categorias atualizados / doc extra pras categorias / tirar os nodes quando clica em um

---
+++ categories.js
@@ -0,0 +1,36 @@
+// array das categorias do filtro
+var categories = [];
+
+// cada categoria é uma posicao no array, e os ids das pessoas vao dentro como outro array
+categories['blue'] = [
+    'ailton-benedito-de-sousa',
+    'alexandre-frota',
+    'bernardo-pires-kuster',
+    'robson-rodovalho',
+    'eduardo-bolsonaro',
+    'joice-hasselmann',
+    'marco-feliciano',
+    'silas-malafaia',
+    'priscila-costa'
+];
+categories['yellow'] = [
+    'ailton-benedito-de-sousa',
+    'alexandre-frota',
+    'damares-alves',
+    'anderson-ferreira',
+    'fernando-francischini',
+    'jair-bolsonaro',
+    'joao-campos',
+    'leandro-ruschel',
+    'priscila-costa',
+];
+categories['red'] = [
+    'ailton-benedito-de-sousa',
+    'eduardo-bolsonaro',
+    'damares-alves',
+    'fernando-francischini',
+    'jair-bolsonaro',
+    'major-vitor-hugo',
+    'silas-malafaia',
+    'rodrigo-constantino',
+];
